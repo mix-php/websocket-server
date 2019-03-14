60a24a9d525950e1dd3fe63907e8f9ec5c528d11
Jian Liu <onanying@126.com>
2019-03-14 16:31:39 +0800

    更新

--- src/WebSocketServer.php
+++ src/WebSocketServer.php
@@ -252,7 +252,7 @@ class WebSocketServer extends AbstractObject
         }
         if ($this->_setting['enable_coroutine'] && Coroutine::id() == -1) {
             xgo(function () use ($server, $fd, $reactorId) {
-                call_user_func([$this, 'onClose'], $server, $fd);
+                call_user_func([$this, 'onClose'], $server, $fd, $reactorId);
             });
             return;
         }
