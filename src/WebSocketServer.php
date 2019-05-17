cbbe0ab13f1e73e2bde64f5829349c69c6b25172
Jian Liu <onanying@126.com>
2019-05-17 19:22:00 +0800

    独立server的公共部分

--- src/WebSocketServer.php
+++ src/WebSocketServer.php
@@ -2,18 +2,25 @@
 
 namespace Mix\WebSocket\Server;
 
-use Mix\Core\Bean\AbstractObject;
 use Mix\Core\Coroutine;
 use Mix\Helper\ProcessHelper;
 use Mix\WebSocket\Frame;
+use Mix\Server\Event;
+use Mix\Server\AbstractServer;
 
 /**
  * Class WebSocketServer
  * @package Mix\WebSocket\Server
  * @author liu,jian <coder.keda@gmail.com>
  */
-class WebSocketServer extends AbstractObject
+class WebSocketServer extends AbstractServer
 {
+
+    /**
+     * 服务名称
+     * @var string
+     */
+    const SERVER_NAME = 'mix-websocketd';
 
     /**
      * 主机
@@ -26,24 +33,6 @@ class WebSocketServer extends AbstractObject
      * @var int
      */
     public $port = 9502;
-
-    /**
-     * 应用配置文件
-     * @var string
-     */
-    public $configFile = '';
-
-    /**
-     * 运行参数
-     * @var array
-     */
-    public $setting = [];
-
-    /**
-     * 服务名称
-     * @var string
-     */
-    const SERVER_NAME = 'mix-websocketd';
 
     /**
      * 默认运行参数
@@ -111,12 +100,6 @@ class WebSocketServer extends AbstractObject
     ];
 
     /**
-     * 服务器
-     * @var \Swoole\WebSocket\Server
-     */
-    public $server;
-
-    /**
      * 启动服务
      * @return bool
      */
@@ -157,90 +140,6 @@ class WebSocketServer extends AbstractObject
     }
 
     /**
-     * 主进程启动事件
-     * 仅允许echo、打印Log、修改进程名称，不得执行其他操作
-     * @param \Swoole\WebSocket\Server $server
-     */
-    public function onStart(\Swoole\WebSocket\Server $server)
-    {
-        // 进程命名
-        ProcessHelper::setProcessTitle(static::SERVER_NAME . ": master {$this->host}:{$this->port}");
-    }
-
-    /**
-     * 主进程停止事件
-     * 请勿在onShutdown中调用任何异步或协程相关API，触发onShutdown时底层已销毁了所有事件循环设施
-     * @param \Swoole\Server $server
-     */
-    public function onShutdown(\Swoole\Server $server)
-    {
-        try {
-
-            // 执行回调
-            $this->setting['hook_shutdown'] and call_user_func($this->setting['hook_shutdown'], $server);
-
-        } catch (\Throwable $e) {
-            // 错误处理
-            \Mix::$app->error->handleException($e);
-        }
-    }
-
-    /**
-     * 管理进程启动事件
-     * 可以使用基于信号实现的同步模式定时器swoole_timer_tick，不能使用task、async、coroutine等功能
-     * @param \Swoole\WebSocket\Server $server
-     */
-    public function onManagerStart(\Swoole\WebSocket\Server $server)
-    {
-        try {
-
-            // 进程命名
-            ProcessHelper::setProcessTitle(static::SERVER_NAME . ": manager");
-            // 执行回调
-            $this->setting['hook_manager_start'] and call_user_func($this->setting['hook_manager_start'], $server);
-
-        } catch (\Throwable $e) {
-            // 错误处理
-            \Mix::$app->error->handleException($e);
-        }
-    }
-
-    /**
-     * 工作进程错误事件
-     * 当Worker/Task进程发生异常后会在Manager进程内回调此函数。
-     * @param \Swoole\Server $server
-     */
-    public function onWorkerError(\Swoole\Server $server, int $workerId, int $workerPid, int $exitCode, int $signal)
-    {
-        try {
-
-            // 执行回调
-            $this->setting['hook_worker_error'] and call_user_func($this->setting['hook_worker_error'], $server, $workerId, $workerPid, $exitCode, $signal);
-
-        } catch (\Throwable $e) {
-            // 错误处理
-            \Mix::$app->error->handleException($e);
-        }
-    }
-
-    /**
-     * 管理进程停止事件
-     * @param \Swoole\WebSocket\Server $server
-     */
-    public function onManagerStop(\Swoole\WebSocket\Server $server)
-    {
-        try {
-
-            // 执行回调
-            $this->setting['hook_manager_stop'] and call_user_func($this->setting['hook_manager_stop'], $server);
-
-        } catch (\Throwable $e) {
-            // 错误处理
-            \Mix::$app->error->handleException($e);
-        }
-    }
-
-    /**
      * 工作进程启动事件
      * @param \Swoole\WebSocket\Server $server
      * @param int $workerId
@@ -259,42 +158,6 @@ class WebSocketServer extends AbstractObject
             $this->setting['hook_worker_start'] and call_user_func($this->setting['hook_worker_start'], $server);
             // 实例化App
             new \Mix\WebSocket\Application(require $this->configFile);
-
-        } catch (\Throwable $e) {
-            // 错误处理
-            \Mix::$app->error->handleException($e);
-        }
-    }
-
-    /**
-     * 工作进程停止事件
-     * @param \Swoole\WebSocket\Server $server
-     * @param int $workerId
-     */
-    public function onWorkerStop(\Swoole\WebSocket\Server $server, int $workerId)
-    {
-        try {
-
-            // 执行回调
-            $this->setting['hook_worker_stop'] and call_user_func($this->setting['hook_worker_stop'], $server);
-
-        } catch (\Throwable $e) {
-            // 错误处理
-            \Mix::$app->error->handleException($e);
-        }
-    }
-
-    /**
-     * 工作进程退出事件
-     * 仅在开启reload_async特性后有效。异步重启特性，会先创建新的Worker进程处理新请求，旧的Worker进程自行退出
-     * @param \Swoole\Server $server
-     */
-    public function onWorkerExit(\Swoole\Server $server, int $workerId)
-    {
-        try {
-
-            // 执行回调
-            $this->setting['hook_worker_exit'] and call_user_func($this->setting['hook_worker_exit'], $server, $workerId);
 
         } catch (\Throwable $e) {
             // 错误处理
@@ -477,37 +340,6 @@ class WebSocketServer extends AbstractObject
                 \Mix::$app->cleanComponents();
             }
         }
-    }
-
-    /**
-     * 欢迎信息
-     */
-    protected function welcome()
-    {
-        $swooleVersion = swoole_version();
-        $phpVersion    = PHP_VERSION;
-        echo <<<EOL
-                             _____
-_______ ___ _____ ___   _____  / /_  ____
-__/ __ `__ \/ /\ \/ /__ / __ \/ __ \/ __ \
-_/ / / / / / / /\ \/ _ / /_/ / / / / /_/ /
-/_/ /_/ /_/_/ /_/\_\  / .___/_/ /_/ .___/
-                     /_/         /_/
-
-
-EOL;
-        println('Server         Name:      ' . static::SERVER_NAME);
-        println('System         Name:      ' . strtolower(PHP_OS));
-        println("PHP            Version:   {$phpVersion}");
-        println("Swoole         Version:   {$swooleVersion}");
-        println('Framework      Version:   ' . \Mix::$version);
-        $this->setting['max_request'] == 1 and println('Hot            Update:    enabled');
-        $this->setting['enable_coroutine'] and println('Coroutine      Mode:      enabled');
-        println("Listen         Addr:      {$this->host}");
-        println("Listen         Port:      {$this->port}");
-        println('Reactor        Num:       ' . $this->setting['reactor_num']);
-        println('Worker         Num:       ' . $this->setting['worker_num']);
-        println("Configuration  File:      {$this->configFile}");
     }
 
 }
